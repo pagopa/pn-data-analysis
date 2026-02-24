9c7b9477fdc1f92ea4623b5e089f58e200b6dc09
Marco Cipollina <marco.cipollina@external.pagopa.it>
2026-02-24 11:52:52 +0100

    Update wi7_Consolidatore.py

--- service_monitoring/consolidatore/wi7/wi7_Consolidatore.py
+++ service_monitoring/consolidatore/wi7/wi7_Consolidatore.py
@@ -1,16 +1,15 @@
 import configparser
-import uuid
 import os
+import uuid
+from datetime import date, datetime, timedelta
 from typing import Dict
-from pyspark.sql.functions import to_date, col
+
 from pyspark import SparkConf
-from pyspark.sql import SparkSession, DataFrameWriter
-from pyspark.sql.functions import udf
-from pyspark.sql.types import StructType, StructField, IntegerType, DateType, StringType
+from pyspark.sql import DataFrameWriter, SparkSession
 from pyspark.sql import functions as F
+from pyspark.sql.functions import ceil, col, to_date, udf, when
+from pyspark.sql.types import DateType, IntegerType, StringType, StructField, StructType
 from pyspark.sql.window import Window
-from datetime import datetime, timedelta, date
-from pyspark.sql.functions import col, ceil, when
 
 spark = SparkSession.builder.getOrCreate()
 
@@ -19,53 +18,64 @@ spark = SparkSession.builder.getOrCreate()
 
 
 festivita = [
-    ('2023-01-01', 'Capodanno'),
-    ('2023-01-06', 'Epifania'),
-    ('2023-04-09', 'Pasqua'),
-    ('2023-04-10', 'Lunedì dell\'Angelo'),
-    ('2023-04-25', 'Festa della Liberazione'),
-    ('2023-05-01', 'Festa dei Lavoratori'),
-    ('2023-06-02', 'Festa della Repubblica'),
-    ('2023-08-15', 'Ferragosto'),
-    ('2023-11-01', 'Tutti i Santi'),
-    ('2023-12-08', 'Immacolata Concezione'),
-    ('2023-12-25', 'Natale'),
-    ('2023-12-26', 'Santo Stefano'),
-
-    ('2024-01-01', 'Capodanno'),
-    ('2024-01-06', 'Epifania'), 
-    ('2024-04-01', 'Lunedì dell\'Angelo'),
-    ('2024-03-31', 'Pasqua'),
-    ('2024-04-25', 'Festa della Liberazione'),
-    ('2024-05-01', 'Festa dei Lavoratori'),
-    ('2024-06-02', 'Festa della Repubblica'),
-    ('2024-08-15', 'Ferragosto'),
-    ('2024-11-01', 'Tutti i Santi'),
-    ('2024-12-25', 'Natale'),
-    ('2024-12-26', 'Santo Stefano'),
-
-    ('2025-01-01', 'Capodanno'),
-    ('2025-01-06', 'Epifania'),
-    ('2025-04-20', 'Pasqua'),
-    ('2025-04-21', 'Lunedì dell\'Angelo'),
-    ('2025-04-25', 'Festa della Liberazione'),
-    ('2025-05-01', 'Festa dei Lavoratori'),
-    ('2025-06-02', 'Festa della Repubblica'),
-    ('2025-08-15', 'Ferragosto'),
-    ('2025-11-01', 'Ognissanti'),
-    ('2025-12-08', 'Immacolata Concezione'),
-    ('2025-12-25', 'Natale'),
-    ('2025-12-26', 'Santo Stefano')
+    ("2023-01-01", "Capodanno"),
+    ("2023-01-06", "Epifania"),
+    ("2023-04-09", "Pasqua"),
+    ("2023-04-10", "Lunedì dell'Angelo"),
+    ("2023-04-25", "Festa della Liberazione"),
+    ("2023-05-01", "Festa dei Lavoratori"),
+    ("2023-06-02", "Festa della Repubblica"),
+    ("2023-08-15", "Ferragosto"),
+    ("2023-11-01", "Tutti i Santi"),
+    ("2023-12-08", "Immacolata Concezione"),
+    ("2023-12-25", "Natale"),
+    ("2023-12-26", "Santo Stefano"),
+    ("2024-01-01", "Capodanno"),
+    ("2024-01-06", "Epifania"),
+    ("2024-04-01", "Lunedì dell'Angelo"),
+    ("2024-03-31", "Pasqua"),
+    ("2024-04-25", "Festa della Liberazione"),
+    ("2024-05-01", "Festa dei Lavoratori"),
+    ("2024-06-02", "Festa della Repubblica"),
+    ("2024-08-15", "Ferragosto"),
+    ("2024-11-01", "Tutti i Santi"),
+    ("2024-12-25", "Natale"),
+    ("2024-12-26", "Santo Stefano"),
+    ("2025-01-01", "Capodanno"),
+    ("2025-01-06", "Epifania"),
+    ("2025-04-20", "Pasqua"),
+    ("2025-04-21", "Lunedì dell'Angelo"),
+    ("2025-04-25", "Festa della Liberazione"),
+    ("2025-05-01", "Festa dei Lavoratori"),
+    ("2025-06-02", "Festa della Repubblica"),
+    ("2025-08-15", "Ferragosto"),
+    ("2025-11-01", "Ognissanti"),
+    ("2025-12-08", "Immacolata Concezione"),
+    ("2025-12-25", "Natale"),
+    ("2025-12-26", "Santo Stefano"),
+    ("2026-01-01", "Capodanno"),
+    ("2026-01-06", "Epifania"),
+    ("2026-04-05", "Pasqua"),
+    ("2026-04-21", "Lunedì dell'Angelo"),
+    ("2026-04-25", "Festa della Liberazione"),
+    ("2026-05-01", "Festa dei Lavoratori"),
+    ("2026-06-02", "Festa della Repubblica"),
+    ("2026-08-15", "Ferragosto"),
+    ("2026-11-01", "Ognissanti"),
+    ("2026-12-08", "Immacolata Concezione"),
+    ("2026-12-25", "Natale"),
+    ("2026-12-26", "Santo Stefano"),
 ]
 
-holiday_dates = {datetime.strptime(date, '%Y-%m-%d').date() for date, _ in festivita}
+holiday_dates = {datetime.strptime(date, "%Y-%m-%d").date() for date, _ in festivita}
 
 festivita_df = spark.createDataFrame(festivita, ["data", "descrizione"])
 
-festivita_df = festivita_df.withColumn("data", to_date(festivita_df["data"], "yyyy-MM-dd"))
+festivita_df = festivita_df.withColumn(
+    "data", to_date(festivita_df["data"], "yyyy-MM-dd")
+)
 
 festivita_df.createOrReplaceTempView("FestivitaView")
-
 
 
 def datediff_workdays(start_date, end_date):
@@ -76,13 +86,19 @@ def datediff_workdays(start_date, end_date):
         if end_date < start_date:
             return 0
 
-        start_date_next_day = start_date.replace(hour=0, minute=0, second=0) + timedelta(days=1)
+        start_date_next_day = start_date.replace(
+            hour=0, minute=0, second=0
+        ) + timedelta(days=1)
 
         total_days = (end_date - start_date_next_day).days
 
-        days_list = [start_date_next_day + timedelta(days=i) for i in range(total_days + 1)]
+        days_list = [
+            start_date_next_day + timedelta(days=i) for i in range(total_days + 1)
+        ]
 
-        working_days = [d for d in days_list if d.weekday() < 5 and d.date() not in holiday_dates]
+        working_days = [
+            d for d in days_list if d.weekday() < 5 and d.date() not in holiday_dates
+        ]
 
         total_working_days = len(working_days)
 
@@ -91,86 +107,135 @@ def datediff_workdays(start_date, end_date):
     except Exception as e:
         return None
 
+
 datediff_workdays_udf = udf(datediff_workdays, IntegerType())
 
 ######################################################## Inizio Query
 
-df_start = spark.sql("""
-    SELECT 
+df_start = spark.sql(
+    """
+    SELECT
         gpa.senderpaid,
-        gpa.requestID, 
-        gpa.requesttimestamp, 
+        gpa.requestID,
+        gpa.requesttimestamp,
         gpa.prodotto,
         gpa.geokey,
-        CASE 
-            WHEN gpa.affido_consolidatore_data IS NULL 
-            THEN gpa.requesttimestamp 
-            ELSE gpa.affido_consolidatore_data 
+        CASE
+            WHEN gpa.affido_consolidatore_data IS NULL
+            THEN gpa.requesttimestamp
+            ELSE gpa.affido_consolidatore_data
         END AS affido_consolidatore_data,
         gpa.stampa_imbustamento_con080_data,
         gpa.materialita_pronta_CON09A_data,
         gpa.affido_recapitista_con016_data,
         gpa.accettazione_recapitista_con018_data
-    FROM 
+    FROM
         send.gold_postalizzazione_analytics AS gpa
-    JOIN 
+    JOIN
         send.gold_notification_analytics AS gna
         ON gpa.iun = gna.iun
-    WHERE 
+    WHERE
         gpa.accettazione_recapitista_con018_data IS NULL
         AND gpa.scarto_consolidatore_stato IS NULL
         AND gpa.pcretry_rank = 1
-        AND gpa.tentativo_recapito_stato IS NULL 
-        AND gpa.messaingiacenza_recapito_stato IS NULL 
-        AND gpa.certificazione_recapito_stato IS NULL 
-        AND gpa.fine_recapito_stato IS NULL 
+        AND gpa.tentativo_recapito_stato IS NULL
+        AND gpa.messaingiacenza_recapito_stato IS NULL
+        AND gpa.certificazione_recapito_stato IS NULL
+        AND gpa.fine_recapito_stato IS NULL
         AND gpa.demat_23l_ar_stato IS NULL
         AND gpa.demat_plico_stato IS NULL
         AND gpa.iun NOT IN (
-            SELECT iun 
+            SELECT iun
             FROM send.silver_incident_iun
         )
-        AND gna.actual_status <> 'CANCELLED' 
-        --- AND gpa.ultimo_evento_stato NOT IN ('P008','P010','P011')          
-        AND gpa.statusrequest NOT IN ('PN999', 'PN998', 'error', 'internalError', 'syntaxError')
-""")
+        AND gna.actual_status <> 'CANCELLED'
+        AND gpa.statusrequest NOT IN ('PN999', 'PN998', 'error', 'internalError', 'syntaxError','transformationError','semanticError','authenticationError', 'duplicatedRequest')
+"""
+)
 
 
-df_start = df_start.withColumn("Cluster",
-                                F.when(F.col("stampa_imbustamento_con080_data").isNull() & 
-                                           (datediff_workdays_udf(F.col("affido_consolidatore_data"),F.current_timestamp()) >= 2), "Stampa Imbustamento") 
-                                 .when(F.col("materialita_pronta_CON09A_data").isNull() & 
-                                       (datediff_workdays_udf(F.col("stampa_imbustamento_con080_data"),F.current_timestamp()) >= 2), "Materialita Pronta")
-                                 .when(F.col("affido_recapitista_con016_data").isNull() &
-                                       (datediff_workdays_udf(F.col("stampa_imbustamento_con080_data"),F.current_timestamp()) >= 2), "Pick Up")
-                                 .when(F.col("accettazione_recapitista_con018_data").isNull() & 
-                                       (datediff_workdays_udf(F.col("affido_recapitista_con016_data"), F.current_timestamp()) >= 1), "Accettazione Recapitista"))
-                                 
+df_start = df_start.withColumn(
+    "Cluster",
+    F.when(
+        F.col("stampa_imbustamento_con080_data").isNull()
+        & (
+            datediff_workdays_udf(
+                F.col("affido_consolidatore_data"), F.current_timestamp()
+            )
+            >= 2
+        ),
+        "Stampa Imbustamento",
+    )
+    .when(
+        F.col("materialita_pronta_CON09A_data").isNull()
+        & (
+            datediff_workdays_udf(
+                F.col("stampa_imbustamento_con080_data"), F.current_timestamp()
+            )
+            >= 2
+        ),
+        "Materialita Pronta",
+    )
+    .when(
+        F.col("affido_recapitista_con016_data").isNull()
+        & (
+            datediff_workdays_udf(
+                F.col("stampa_imbustamento_con080_data"), F.current_timestamp()
+            )
+            >= 2
+        ),
+        "Pick Up",
+    )
+    .when(
+        F.col("accettazione_recapitista_con018_data").isNull()
+        & (
+            datediff_workdays_udf(
+                F.col("affido_recapitista_con016_data"), F.current_timestamp()
+            )
+            >= 1
+        ),
+        "Accettazione Recapitista",
+    ),
+)
+
 df_start = df_start.filter(F.col("Cluster").isNotNull())
 
-df_start = df_start.withColumn("Priority", 
-                                F.when(F.col("Cluster") == "Stampa Imbustamento", 4)
-                                 .when(F.col("Cluster") == "Materialita Pronta", 3)
-                                 .when(F.col("Cluster") == "Pick Up", 2)
-                                 .when(F.col("Cluster") == "Accettazione Recapitista", 1))
-								 
-								 
+df_start = df_start.withColumn(
+    "Priority",
+    F.when(F.col("Cluster") == "Stampa Imbustamento", 4)
+    .when(F.col("Cluster") == "Materialita Pronta", 3)
+    .when(F.col("Cluster") == "Pick Up", 2)
+    .when(F.col("Cluster") == "Accettazione Recapitista", 1),
+)
+
+
 # Calcolo della differenza tra gg lavorativi start date - end date
 df_computed = df_start.withColumn(
     "differenza_gg_lavorativi",
     F.when(
         F.col("Cluster") == "Stampa Imbustamento",
-        datediff_workdays_udf(F.col("affido_consolidatore_data"), F.current_timestamp())
-    ).when(
-        F.col("Cluster") == "Materialita Pronta",
-        datediff_workdays_udf(F.col("stampa_imbustamento_con080_data"), F.current_timestamp())
-    ).when(
-        F.col("Cluster") == "Pick Up",
-        datediff_workdays_udf(F.col("stampa_imbustamento_con080_data"), F.current_timestamp())
-    ).when(
-        F.col("Cluster") == "Accettazione Recapitista",
-        datediff_workdays_udf(F.col("affido_recapitista_con016_data"), F.current_timestamp())
+        datediff_workdays_udf(
+            F.col("affido_consolidatore_data"), F.current_timestamp()
+        ),
     )
+    .when(
+        F.col("Cluster") == "Materialita Pronta",
+        datediff_workdays_udf(
+            F.col("stampa_imbustamento_con080_data"), F.current_timestamp()
+        ),
+    )
+    .when(
+        F.col("Cluster") == "Pick Up",
+        datediff_workdays_udf(
+            F.col("stampa_imbustamento_con080_data"), F.current_timestamp()
+        ),
+    )
+    .when(
+        F.col("Cluster") == "Accettazione Recapitista",
+        datediff_workdays_udf(
+            F.col("affido_recapitista_con016_data"), F.current_timestamp()
+        ),
+    ),
 )
 
 # Calcolo dei giorni effettivi fuori SLA
@@ -178,17 +243,30 @@ df_computed = df_computed.withColumn(
     "gg_fuori_sla",
     F.when(
         F.col("Cluster") == "Stampa Imbustamento",
-        datediff_workdays_udf(F.col("affido_consolidatore_data"), F.current_timestamp()) - 2 
-    ).when(
-        F.col("Cluster") == "Materialita Pronta",
-        datediff_workdays_udf(F.col("stampa_imbustamento_con080_data"), F.current_timestamp()) - 2 
-    ).when(
-        F.col("Cluster") == "Pick Up",
-        datediff_workdays_udf(F.col("stampa_imbustamento_con080_data"), F.current_timestamp()) - 2
-    ).when(
-        F.col("Cluster") == "Accettazione Recapitista",
-        datediff_workdays_udf(F.col("affido_recapitista_con016_data"), F.current_timestamp()) - 1 
+        datediff_workdays_udf(F.col("affido_consolidatore_data"), F.current_timestamp())
+        - 2,
     )
+    .when(
+        F.col("Cluster") == "Materialita Pronta",
+        datediff_workdays_udf(
+            F.col("stampa_imbustamento_con080_data"), F.current_timestamp()
+        )
+        - 2,
+    )
+    .when(
+        F.col("Cluster") == "Pick Up",
+        datediff_workdays_udf(
+            F.col("stampa_imbustamento_con080_data"), F.current_timestamp()
+        )
+        - 2,
+    )
+    .when(
+        F.col("Cluster") == "Accettazione Recapitista",
+        datediff_workdays_udf(
+            F.col("affido_recapitista_con016_data"), F.current_timestamp()
+        )
+        - 1,
+    ),
 )
 
 df_filtered = df_computed.filter(F.col("gg_fuori_sla") > 0)
@@ -198,12 +276,11 @@ df_filtered = df_computed.filter(F.col("gg_fuori_sla") > 0)
 df_filtered.createOrReplaceTempView("DF_OUTPUT")
 
 
-spark.sql("""SELECT * FROM DF_OUTPUT""").writeTo("send_dev.wi7_consolidatore")\
-                .using("iceberg")\
-                .tableProperty("format-version","2")\
-                .tableProperty("engine.hive.enabled","true")\
-                .createOrReplace()
-#print(datetime.now()-start)
+spark.sql("""SELECT * FROM DF_OUTPUT""").writeTo("send_dev.wi7_consolidatore").using(
+    "iceberg"
+).tableProperty("format-version", "2").tableProperty(
+    "engine.hive.enabled", "true"
+).createOrReplace()
+# print(datetime.now()-start)
 
 spark.stop()
-                                 
